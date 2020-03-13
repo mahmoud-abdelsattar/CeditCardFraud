a6459ec683acd3323cc977530f5cdd626ef8ec9d
Mahmoud Abd Elsattar <mahmoud-8@windowslive.com>
2020-03-13 23:54:47 +0200

    commit from phcharm

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 from routes import app
-=======
-from flask import Flask
-import routes
-from routes import app
-
->>>>>>> origin/master
 
 if __name__ == "__main__":
     app.run(debug=True)
